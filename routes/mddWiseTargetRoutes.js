276c838cbcbae33c506cdb7aee86cb0c1cd67148
Rakshita Kanwar <rakshitakanwar09@gmail.com>
2025-09-19 18:52:08 +0530

    what

--- routes/mddWiseTargetRoutes.js
+++ routes/mddWiseTargetRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const upload = require("../helpers/multerHelper");
 const { adminOrSuperAdminAuth, userAuth } = require("../middlewares/authmiddlewares");
-const { getMddWiseTargets, uploadMddWiseTargets } = require("../controllers/admin/mddWiseTargetControllerNew");
+// const { getMddWiseTargets, uploadMddWiseTargets } = require("../controllers/admin/mddWiseTargetControllerNew");
 const router = express.Router();
 
-router.post("/upload/mdd-wise-targets", upload.single("file"), userAuth, uploadMddWiseTargets);
-router.get("/get/mdd-wise-targets", userAuth, getMddWiseTargets);
+// router.post("/upload/mdd-wise-targets", upload.single("file"), userAuth, uploadMddWiseTargets);
+// router.get("/get/mdd-wise-targets", userAuth, getMddWiseTargets);
 
 
 module.exports = router;
