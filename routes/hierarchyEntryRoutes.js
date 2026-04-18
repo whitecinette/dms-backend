cbc7ab83cc57de6c5bc971e6d7bbf73b80e0750c
rakshitaaa <rakshitakanwar09@gmail.com>
2026-04-18 14:45:53 +0530

    hierarchy update

--- routes/hierarchyEntryRoutes.js
+++ routes/hierarchyEntryRoutes.js
@@ -10,32 +10,28 @@ const {
   bulkUpdateHierarchyEntries,
 } = require("../controllers/new/hierarchyEntryController");
 
-// META
 router.get(
   "/super-admin/hierarchy/meta",
   superAdminAuth,
   getHierarchyMeta
 );
 
-// LIST
 router.get(
   "/super-admin/hierarchy",
   superAdminAuth,
   getHierarchyEntries
 );
 
-// SINGLE UPDATE
-router.patch(
-  "/super-admin/hierarchy/:id",
-  superAdminAuth,
-  updateHierarchyEntry
-);
-
-// BULK UPDATE
 router.patch(
   "/super-admin/hierarchy/bulk-update",
   superAdminAuth,
   bulkUpdateHierarchyEntries
+);
+
+router.patch(
+  "/super-admin/hierarchy/:id",
+  superAdminAuth,
+  updateHierarchyEntry
 );
 
 module.exports = router;
